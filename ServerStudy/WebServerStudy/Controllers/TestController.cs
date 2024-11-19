f14713327dc374adb6348357b1799366b0d8d802
DESKTOP-Q8290FB\Major <majorgking@gmail.com>
2024-11-19 19:34:03 +0900

    ASP.Net Web API core 및 post테스트용 서버 추가

---
+++ ServerStudy/WebServerStudy/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebServerStudy.Controllers
+{
+    [Route("test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // POST api/<TestController>
+        [HttpPost]
+        [Route("hello")]
+        public TestPacketRes TestPost([FromBody] TestPacketReq value)
+        {
+            TestPacketRes result = new TestPacketRes();
+            result.success = true;
+
+            return result;
+        }
+    }
+}
