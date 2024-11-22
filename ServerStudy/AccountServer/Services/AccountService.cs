d25983b619724b3d4666aee7ce2813579999306d
DESKTOP-Q8290FB\Major <majorgking@gmail.com>
2024-11-22 09:28:12 +0900

    기존 서버 부분 삭제 및 페이스로그인 관련 웹 서버 추가

---
+++ ServerStudy/AccountServer/Services/AccountService.cs
@@ -0,0 +1,68 @@
+﻿using AccountDB;
+using AccountServer.Data;
+
+
+namespace AccountServer.Services
+{
+    public class AccountService
+    {
+        AccountDbContext _dbContext;
+        FacebookService _facebook;
+
+        public AccountService(AccountDbContext context, FacebookService facebook)
+        {
+            _dbContext = context;
+            _facebook = facebook;
+        }
+
+        public async Task<LoginAccountPacketRes> LoginFacebookAccount(string token)
+        {
+            LoginAccountPacketRes res = new LoginAccountPacketRes();
+
+            FacebookTokenData? tokenData = await _facebook.GetUserTokenData(token);
+            if (tokenData == null || tokenData.is_valid == false)
+                return res;
+
+            AccountDb? accountDb = _dbContext.Accounts.FirstOrDefault(a => a.LoginProviderUserId == tokenData.user_id && a.LoginProviderType == ProviderType.Facebook);
+            if (accountDb == null)
+            {
+                accountDb = new AccountDb()
+                {
+                    LoginProviderUserId = tokenData.user_id,
+                    LoginProviderType = ProviderType.Facebook
+                };
+
+                _dbContext.Accounts.Add(accountDb);
+                await _dbContext.SaveChangesAsync();
+            }
+
+            res.success = true;
+            res.accountDbId = accountDb.AccountDbId;
+
+            return res;
+        }
+
+        public async Task<LoginAccountPacketRes> LoginGuestAccount(string userID)
+        {
+            LoginAccountPacketRes res = new LoginAccountPacketRes();
+
+            AccountDb? accountDb = _dbContext.Accounts.FirstOrDefault(a => a.LoginProviderUserId == userID && a.LoginProviderType == ProviderType.Guest);
+            if (accountDb == null)
+            {
+                accountDb = new AccountDb()
+                {
+                    LoginProviderUserId = userID,
+                    LoginProviderType = ProviderType.Guest
+                };
+
+                _dbContext.Accounts?.Add(accountDb);
+                await _dbContext.SaveChangesAsync();
+            }
+
+            res.success = true;
+            res.accountDbId = accountDb.AccountDbId;
+
+            return res;
+        }
+    }
+}
